9fdab54430a12544fac638dbed5d9870d50a030a
unknown <sveinni19@ru.is>
2020-03-10 16:27:19 +0000

    Green

--- sample.py
+++ sample.py
@@ -1,5 +1,15 @@
 def Fizzbuzz(number):
-    if number % 5 == True:
+    if number % 5 == 0 and number % 3 == 0:
         return "Fizbuzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return number
 
-print(Fizzbuzz(100))
+
+print(Fizzbuzz(6))
+print(Fizzbuzz(15))
+print(Fizzbuzz(10))
+print(Fizzbuzz(7))
